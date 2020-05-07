bf55c734ea7335d9ac5f48f2d6117798d6e36725
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-05-07 21:30:41 +0200

    deploy

--- api/api.py
+++ api/api.py
@@ -2,8 +2,12 @@ import datetime
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../build",static_url_path="/")
 cors = CORS(app)
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/api/time')
 def get_current_time():
