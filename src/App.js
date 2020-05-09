198a4694df4dba21ba4e419dce9e8342a0a63afa
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-05-09 12:27:45 +0200

    added multiple routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,21 +7,29 @@ function App() {
   const [message, setMessage] = useState("Waiting...");
 
   useEffect(() => {
-    fetch('api/message').then(res => res.json()).then(data => {
-      setMessage(data.message);
-    });
-  },[]);
+    fetch("api/message")
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(data.message);
+      });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          The message is: {message}
-        </p>
+      <img src={logo} className="App-logo" alt="logo" />
+        <ul className="mylist">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <p>The message is: {message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <App/>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </div>
+  </Router>,
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
